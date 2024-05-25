a15d8bb93058ed7cc5eef848edda7698fce7f9b3
dbolanos <dbolanos@isppro.net>
2024-05-24 18:34:12 -0600

    Add Transaction Entity

---
+++ Entities/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccountAPI.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        public string TransactionType { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor a cero.")]
+        public double Amount { get; set; }
+    }
+}
