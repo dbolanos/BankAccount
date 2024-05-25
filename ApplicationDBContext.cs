9372e3a7f2ea1f1e05f0e4b78ce92aef23f5c930
dbolanos <dbolanos@isppro.net>
2024-05-24 18:44:44 -0600

    Add Entity/Interest and changes in ApplicationDBContext

--- ApplicationDBContext.cs
+++ ApplicationDBContext.cs
@@ -17,5 +17,7 @@ namespace BankAccountAPI
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Account> Accounts { get; set; }
+        public DbSet<Interest> Interests { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
---
+++ Entities/Interest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankAccountAPI.Entities
+{
+    public class Interest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public Account Account { get; set; }
+    }
+}
